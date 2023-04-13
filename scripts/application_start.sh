3a49ee58169523c2a887478b4a031b61f7411370
ElephontDearie <sonia.hussain144@gmail.com>
2023-04-13 23:21:00 +0100

    Sanity check on node version

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/away-model-board
+which node
+sudo npm i -g npm@latest
+which node
 git pull
+rm -rf node_modules yarn.lock
 yarn install &&
 yarn build &&
 pm2 restart away-model-tracker-board
