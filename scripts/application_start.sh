e26c2ea54674ec0a447f407a74410b4b2acc343f
ElephontDearie <sonia.hussain144@gmail.com>
2023-04-14 13:12:24 +0100

    Upgrade node version if not already installed

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,10 +5,15 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-source ~/.bashrc
+# source ~/.bashrc
 nvm -v
-nvm install 18.15.0
-nvm alias default 18.15.0
+
+if [[ $(nvm ls | grep v18.15.0) == "" ]]; then
+    nvm install v18.15.0
+else
+    nvm use v18.15.0
+fi
+
 node -v
 git pull
 rm -rf node_modules yarn.lock
