5702d4cbd4e971ec6a971d09b74816a39c4b1748
ElephontDearie <sonia.hussain144@gmail.com>
2023-04-15 01:33:42 +0100

    Install NVM always as NVM_DIR not exported correctly

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/away-model-board
 
-if [[ $NVM_DIR == " " ]]; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-else
-    echo "NVM is already installed. Skipping installation"
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 if [[ $(nvm ls | grep v18.15.0) == "" ]]; then
     nvm install v18.15.0
