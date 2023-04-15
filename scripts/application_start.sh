6b8d5cbe0190780631cbdbb6d4f5bb433fc2fe9b
ElephontDearie <sonia.hussain144@gmail.com>
2023-04-15 15:00:29 +0100

    Export NVM_DIR for codedeploy build

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,10 +5,11 @@ if [ -d "${HOME}/.nvm/.git" ]; then
     echo "nvm installed"
 else 
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 if [[ $(nvm ls | grep v18.15.0) == "" ]]; then
     nvm install v18.15.0
