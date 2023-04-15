685d53238e0a71fc3d4a4a7a7efe175bbab0f60b
ElephontDearie <sonia.hussain144@gmail.com>
2023-04-15 01:27:39 +0100

    Fix spacing in bash app_start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/away-model-board
 
-if [[ $NVM_DIR == " "]]; then
+if [[ $NVM_DIR == " " ]]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
