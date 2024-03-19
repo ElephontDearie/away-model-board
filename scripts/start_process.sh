fec78888ea1dd7d6af12e4a112b9835be6031b1d
ElephontDearie <sonia.hussain144@gmail.com>
2024-03-19 12:47:43 +0000

    Add user permissions and fix format errors in start script

--- scripts/start_process.sh
+++ scripts/start_process.sh
@@ -1,10 +1,12 @@
 #!/bin/bash 
-container_name=name
+container_name=away-tracker
 
 #Install and run docker daemon
 sudo yum -y install docker
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+sudo chmod 666 /var/run/docker.sock
 
 #ECR Login
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 992382626730.dkr.ecr.us-east-1.amazonaws.com
@@ -12,9 +14,10 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker pull 992382626730.dkr.ecr.us-east-1.amazonaws.com/away-tracker:latest
 
 ##Changing image tag
-docker image tag 992382626730..dkr.ecr.us-east-1.amazonaws.com/away-tracker:latest $container_name:latest
+docker image tag 992382626730.dkr.ecr.us-east-1.amazonaws.com/away-tracker:latest $container_name:latest
 
-#stop and remove the current container docker rm -f $container_name
+#stop and remove the current container 
+docker rm -f $container_name
 
 #Creating and starting a docker container using a new image
 docker run -d -p 80:80 --name $container_name $container_name:latest
